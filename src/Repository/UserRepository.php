1291ddc65a71f9f1c8fe3960ef34c5eb22e5c7f3
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-01-15 23:29:00 +0100

    Add find user by email and role

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * Get users by the role.
+     * @param $userRoleId
+     * The role id to search users.
+     * @return null|User
+     */
+    public function findByRoleId($userRoleId)
+    {
+        $qb = $this->createQueryBuilder('user')
+                    ->select('user')
+                    ->andWhere('user.role_id == :userRole')
+                    ->andWhere("user.active == '1'")
+                    ->setParameter('userRole', $userRoleId)
+                    ->getQuery();
+
+        return $qb->getResult();
+    }
+
+    /**
+     * Get user by the email.
+     * @param $userEmail
+     * The user email to find the user.
+     * @return null|User
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findByEmail($userEmail)
+    {
+        $qb = $this->createQueryBuilder('user')
+                    ->select('user')
+                    ->Where('user.email == :email')
+                    ->setParameter('email', $userEmail)
+                    ->getQuery();
+
+        return $qb->setMaxResults(1)
+                  ->getOneOrNullResult();
+    }
+}
