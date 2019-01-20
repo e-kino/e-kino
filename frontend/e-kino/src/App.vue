e82b4a59a882832c07637a9084737cef9a37060a
Bartosz <26346113+bbasinski@users.noreply.github.com>
2019-01-20 14:16:20 +0100

    Search in home

--- frontend/e-kino/src/App.vue
+++ frontend/e-kino/src/App.vue
@@ -18,33 +18,7 @@
         </div>
       </div>
     </nav>
-    <section class="hero is-info">
-      <div class="hero-body">
-        <div class="container">
-          <div class="card">
-            <div class="card-content">
-              <div class="content">
-                <div class="control has-icons-left has-icons-right">
-                  <label for="search">Wyszukaj seanse</label>
-                  <input id="search" class="input is-large" type="date"/>
-                  <span class="icon is-medium is-left">
-                            <i class="fa fa-search"></i>
-                        </span>
-                  <span class="icon is-medium is-right">
-                            <i class="fa fa-empire"></i>
-                        </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="container">
-      <div class="level-item">
-        <router-view/>
-      </div>
-    </section>
+    <router-view/>
     <div class="columns is-mobile is-centered">
       <div class="column is-half is-narrow"></div>
     </div>
--- frontend/e-kino/src/views/Home.vue
+++ frontend/e-kino/src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="home">
+    <section class="hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <div class="card">
+            <div class="card-content">
+              <div class="content">
+                <div class="control has-icons-left has-icons-right">
+                  <label for="search">Wyszukaj seanse</label>
+                  <input id="search" class="input is-large" type="date"/>
+                  <span class="icon is-medium is-left">
+                            <i class="fa fa-search"></i>
+                        </span>
+                  <span class="icon is-medium is-right">
+                            <i class="fa fa-empire"></i>
+                        </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="container">
+      <div class="level-item">
+asd
+      </div>
+    </section>
   </div>
 </template>
 
