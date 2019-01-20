c4a1218b555b28683d9c7a8fb69778c9413fa750
Bartosz <26346113+bbasinski@users.noreply.github.com>
2019-01-20 14:36:46 +0100

    Screening component

---
+++ frontend/e-kino/src/components/Screening.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="column is-one-quarter-desktop is-half-tablet">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-3by2">
+          <img src="https://unsplash.it/300/200/?random&pic=2" alt="">
+        </figure>
+        <div class="card-content is-overlay is-clipped">
+              <span class="tag is-info">
+                Tytuł filmu
+              </span>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a class="card-footer-item">
+          Rezerwuj
+        </a>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Screening"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/e-kino/src/views/Home.vue
+++ frontend/e-kino/src/views/Home.vue
@@ -23,19 +23,35 @@
       </div>
     </section>
     <section class="container">
-      <div class="level-item">
-asd
+      <div class="columns is-multiline" style="margin-top: 30px">
+        <screening></screening>
+        <screening></screening>
+        <screening></screening>
+        <screening></screening>
+        <screening></screening>
+        <screening></screening>
+        <screening></screening>
+        <screening></screening>
+        <screening></screening>
+
       </div>
     </section>
   </div>
 </template>
 
 <script>
+  import Screening from '@/components/Screening'
 
-export default {
-  name: 'home',
-  components: {
-
-  },
-};
+  export default {
+    name: 'home',
+    components: {
+      Screening
+    },
+  };
 </script>
+
+<style scoped>
+  .has-padding-20 {
+    padding: 20px !important;
+  }
+</style>
