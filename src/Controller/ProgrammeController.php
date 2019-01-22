f05ed3c58b8a71bc5c1d2fd3d4c6b82640951a42
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-01-22 22:02:13 +0100

    Add get all programmes

--- src/Controller/ProgrammeController.php
+++ src/Controller/ProgrammeController.php
@@ -2,13 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Programme;
+use App\Repository\ProgrammeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProgrammeController extends AbstractController
 {
+    /**
+     * @Route("/programmes", name="ekino_get_all_programmes")
+     * @return JsonResponse
+     * @throws \LogicException
+     */
     public function getAll()
     {
-        //todo DW
+        /** @var ProgrammeRepository $programmeRepository */
+        $programmeRepository = $this->getDoctrine()
+            ->getRepository(Programme::class);
 
+        $programmes = $programmeRepository->findAll();
+
+        if (empty($programmes)) {
+            return new JsonResponse(['error' => [
+                'code' => 400,
+                'message' => "Nie znaleziono programow"
+            ]]);
+        }
+
+        return new JsonResponse([
+            'programmes' => $programmes
+        ]);
     }
 }
