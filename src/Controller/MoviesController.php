571b3882530b3c973f648f0721997857d154dfdd
dwiecko <dwiecko@sigma.ug.edu.pl>
2019-01-22 22:30:59 +0100

    Add remove movie by id

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -40,13 +41,60 @@ class MoviesController extends AbstractController
     }
 
     /**
-     * @Route("/movies/{id}", methods={"DELETE"}, name="ekino_delete_movies")
+     * @Route("/movies/{$id}", name="ekino_get_a_movie_by_id")
      * @param $id
+     * @return JsonResponse
+     * @throws \LogicException
+     */
+    public function getMovieById($id)
+    {
+        /** @var MovieRepository $movieRepository */
+        $movieRepository = $this->getDoctrine()
+            ->getRepository(Movie::class);
+
+        $movie = $movieRepository -> find($id);
+
+        if(empty($movie) || empty($id)){
+            return new JsonResponse(['error' => [
+                'code' => 400,
+                'message' => "Nie znaleziono filmu"
+            ]]);
+        }
+
+        return new JsonResponse([
+            'movie' => $movie
+        ]);
+    }
+
+    /**
+     * @Route("/movies/delete/{id}", name="ekino_delete_movies")
+     * @param $id
+     * @return JsonResponse
+     * @throws \Doctrine\ORM\ORMException
      */
     public function deleteMovie($id)
     {
-        //todo DW
-        //todo
+        /** @var MovieRepository $movieRepository */
+        $movieRepository = $this->getDoctrine()
+            ->getRepository(Movie::class);
+
+        $movie = $movieRepository -> find($id);
+
+        $entityManager = $this -> getDoctrine() -> getManager();
+
+        try{
+            $entityManager -> remove($movie);
+
+            $entityManager -> flush();
+        }
+        catch (\Exception $exception){
+            return new JsonResponse(['error' =>  "Nie mozna usunac podanego filmu." + $exception],
+                400);
+        }
+
+        return new JsonResponse([
+            'message' => "Pomyslnie usunieto film."
+        ]);
     }
 
     /**
