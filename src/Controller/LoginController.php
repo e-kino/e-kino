4116767c1893a006e6c8b1f94ba4a9fe0a0e893c
tom_ftb <tborczynski87@gmail.com>
2019-01-23 12:07:10 +0100

    ADD NEW FUNCTION  loginGet AND logoutGet . Create response to try use methon GET, and return 405 error code

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LoginController extends AbstractController
 {
     /**
      * @Route("/login", methods={"POST"}, name="ekino_login")
      * @param Request $request
+     * @return JsonResponse
      */
     public function login(Request $request)
     {
+        var_dump($request);
         //todo TB git test
         //todo get json from request
         //todo create new session
@@ -31,5 +35,27 @@ class LoginController extends AbstractController
     protected function setSession($userData)
     {
         // SET SESSION 
+    }
+    /**
+     * @Route("/login", methods={"GET"}, name="ekino_login")
+     * @return JsonResponse
+     */
+    public function loginGet()
+    {
+        return new JsonResponse(['error' => [
+                'code' => 405,
+                'message' => "Method not allowed you ugly buster"
+            ]]);
+    }
+    /**
+     * @Route("/logout", methods={"GET"}, name="ekino_logout")
+     * @return JsonResponse
+     */
+    public function logoutGet()
+    {
+        return new JsonResponse(['error' => [
+                'code' => 405,
+                'message' => "Method not allowed you ugly buster"
+            ]]);
     }
 }
