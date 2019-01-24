69fe3e13a3ee0018988fb45526fb81825ebb6388
tom_ftb <tborczynski87@gmail.com>
2019-01-24 16:46:43 +0100

    ADD register via METHOD GET. RETURN NOT ALLOWED

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -11,12 +11,26 @@ class RegisterController extends AbstractController
     /**
      * @Route("/register", methods={"POST"} name="ekino_register")
      * @param Request $request
+     * @return JsonResponse
      */
-    //public function register(Request $request)
-    //{
+    public function register(Request $request)
+    {
         //todo TB
 
         //todo get json from request
         //todo create new user and return it
-    //}
+    }
+    /**
+     * @Route("/register", methods={"GET"} name="ekino_register_get")
+     * @return JsonResponse
+     */
+    public function registerGet()
+    {
+        return new JsonResponse([
+            'error' => [
+                'code' => 405,
+                'message' => "Method not allowed you ugly buster"
+            ]
+        ]);
+    }
 }
