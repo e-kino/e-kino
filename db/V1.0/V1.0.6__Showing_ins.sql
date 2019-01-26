17665bb73e96c6a50c2d02cfa48d95c2cf4d03c0
tborczynski87@gmail.com <tborczynski87@gmail.com>
2019-01-26 21:26:46 +0100

    SHOWIN INS RECORDS

---
+++ db/V1.0/V1.0.6__Showing_ins.sql
@@ -0,0 +1,67 @@
+/*
+-- Query: SELECT * FROM ekino.showing
+LIMIT 0, 1000
+
+-- Date: 2019-01-26 21:19
+*/
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','10:00:00',1,1);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','12:00:00',1,1);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','14:00:00',1,1);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','16:00:00',1,1);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','11:00:00',1,2);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:00:00',1,2);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','15:00:00',1,2);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,2);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','11:00:00',1,3);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:00:00',1,3);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','15:00:00',1,3);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,3);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','10:00:00',1,4);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:00:00',1,4);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,4);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','21:00:00',1,4);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','12:00:00',1,5);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','14:00:00',1,5);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','17:00:00',1,5);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','19:00:00',1,2);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','09:00:00',1,6);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','12:00:00',1,6);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','15:00:00',1,6);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,6);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','11:00:00',1,7);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:00:00',1,7);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','20:00:00',1,7);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','22:00:00',1,7);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','14:00:00',1,8);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','16:00:00',1,8);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,8);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','20:30:00',1,8);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','11:00:00',1,9);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:00:00',1,9);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','15:00:00',1,9);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,9);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','11:00:00',1,10);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:00:00',1,10);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','15:00:00',1,10);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,10);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','10:30:00',1,11);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:30:00',1,11);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','16:30:00',1,11);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','19:30:00',1,11);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','12:00:00',1,12);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','14:00:00',1,12);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','17:00:00',1,12);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','19:00:00',1,12);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','09:00:00',1,13);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','12:00:00',1,13);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','15:00:00',1,13);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','18:00:00',1,13);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','11:00:00',1,14);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:00:00',1,14);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','20:00:00',1,14);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','22:00:00',1,14);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','10:30:00',1,15);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','13:30:00',1,15);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','15:30:00',1,15);
+INSERT INTO ekino.`showing` (`date_add`,`time_show`,`programme_id`,`movie_id`) VALUES ('2019-01-26 08:00:00','20:30:00',1,15);
+
