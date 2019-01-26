f5ae00cb1548069a1ed2c0bc7efc87350d415137
tborczynski87@gmail.com <tborczynski87@gmail.com>
2019-01-26 21:01:50 +0100

    PROGRAMME INSERT RECORDS

---
+++ db/V1.0/V1.0.5__Programme_ins.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM ekino.programme
+LIMIT 0, 1000
+
+-- Date: 2019-01-26 21:00
+*/
+INSERT INTO ekino.`programme` (`id`,`date_add`,`date_start`,`date_end`) VALUES (1,'2019-01-26 08:00:00','2019-01-26 08:00:00','2019-02-26 23:59:59');
