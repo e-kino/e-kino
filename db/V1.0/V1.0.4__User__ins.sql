2501e519e19bc2a4681bb724ae2b11af2a90626e
tborczynski87@gmail.com <tborczynski@DDI1374TB.geofizyka.pl>
2019-01-26 20:56:36 +0100

    USER INSERT RECORDS

---
+++ db/V1.0/V1.0.4__User__ins.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM ekino.user
+LIMIT 0, 1000
+DEF PASS demo
+-- Date: 2019-01-26 20:47
+*/
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('1','tomek@ekino.pl','$2a$10$mLPW8yRM6TV.jIZY9X2M1.8/RleHg/gOIuHbft59V/0aTTuFA6mvy','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('1','bartek@ekino.pl','$2a$10$u0HpBiwZxG7zSWnNyTxpQO5mr2ILoFMATIb/KD0F54Xr8VmkPkyx6','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('1','dawid@ekino.pl','$2a$10$8Nptqbf8QUH6QfsUT2ohYOUVSXbyR7Pqtb6nUCX18kkx1rcCHhaxS','2019-01-01 08:00:00',1);
+
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('0','alina@wp.pl','$2a$10$75b2vL4tvFfIM2z0.vwoi.8/qRoQ0L37DnE2sDxRtrXQG8Pgr22A.','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('0','dark@onet.pl','$2a$10$75b2vL4tvFfIM2z0.vwoi.8/qRoQ0L37DnE2sDxRtrXQG8Pgr22A.','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('0','redo@hierens.com.pl','$2a$10$75b2vL4tvFfIM2z0.vwoi.8/qRoQ0L37DnE2sDxRtrXQG8Pgr22A.','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('0','tree@prolog.com.pl','$2a$10$75b2vL4tvFfIM2z0.vwoi.8/qRoQ0L37DnE2sDxRtrXQG8Pgr22A.','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('0','io@smart.com','$2a$10$75b2vL4tvFfIM2z0.vwoi.8/qRoQ0L37DnE2sDxRtrXQG8Pgr22A.','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('0','kasia.kowalska@gmail.com','$2a$10$75b2vL4tvFfIM2z0.vwoi.8/qRoQ0L37DnE2sDxRtrXQG8Pgr22A.','2019-01-01 08:00:00',1);
+INSERT INTO ekino.`user` (`role_id`,`email`,`password`,`date_add`,`active`) VALUES ('0','jacek.michnik@neostrada.pl','$2a$10$75b2vL4tvFfIM2z0.vwoi.8/qRoQ0L37DnE2sDxRtrXQG8Pgr22A.','2019-01-01 08:00:00',1);
